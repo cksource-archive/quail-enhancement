afd989188c81e9897c33bee3e2cd50605b31d001
Marek Lewandowski <m.lewandowski@cksource.com>
2015-03-09 14:39:40 +0100

    Introduced a suggested config.

---
+++ suggestedConfig.js
@@ -0,0 +1,18 @@
+// Below a resaonable Quail Config.
+
+var assessments = [
+	'tableDataShouldHaveTh',
+	'fontIsNotUsed',
+	'iIsNotUsed',
+	'basefontIsNotUsed',
+	'boldIsNotUsed',
+	'pNotUsedAsHeader',
+	'headersHaveText',
+	'tableComplexHasSummary',
+	'tableUsesCaption', // Only after fixes mentioned in H39.
+	'tableCaptionIdentifiesTable',
+	'listOfLinksUseList',
+	// 'documentVisualListsAreMarkedUp', // Would be handy if it would support <br /> with attributes.
+	// `definitionListsAreUsed`, // Would be handy if only we'd support dl/dt/dd.
+	
+];
