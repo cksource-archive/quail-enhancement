85863a8f46c1a98f17f43452d8ad6f192b630fc9
Marek Lewandowski <m.lewandowski@cksource.com>
2015-03-24 12:34:08 +0100

    Updated suggested config.

--- suggestedConfig.js
+++ suggestedConfig.js
@@ -13,8 +13,9 @@ var assessments = [
 	'tableCaptionIdentifiesTable',
 	'listOfLinksUseList',
 	'documentVisualListsAreMarkedUp', // Would be handy if it would support <br /> with attributes. But it's fixed in [#319](https://github.com/quailjs/quail/pull/319)!
-	// `definitionListsAreUsed`, // Would be handy if only we'd support dl/dt/dd.
+	// 'definitionListsAreUsed', // Would be handy if only we'd support dl/dt/dd.
 	'tableUsesScopeForRow',
 	'tableWithBothHeadersUseScope',
 	// 'tableDataShouldHaveTh', // Only after fixed mentioned in F91.
+	// 'documentContentReadableWithoutStylesheets', // Only after fixes mentioned in G140.
 ];
