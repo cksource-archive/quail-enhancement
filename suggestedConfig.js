4fd31751cf4e5e4ae0f4c56e0c02574554a7fd92
Marek Lewandowski <m.lewandowski@cksource.com>
2015-03-24 10:51:27 +0100

    Actually we can start suggesting `documentVisualListsAreMarkedUp` test, as it has been patched in Quail master.

--- suggestedConfig.js
+++ suggestedConfig.js
@@ -12,7 +12,7 @@ var assessments = [
 	'tableUsesCaption', // Only after fixes mentioned in H39.
 	'tableCaptionIdentifiesTable',
 	'listOfLinksUseList',
-	// 'documentVisualListsAreMarkedUp', // Would be handy if it would support <br /> with attributes.
+	'documentVisualListsAreMarkedUp', // Would be handy if it would support <br /> with attributes. But it's fixed in [#319](https://github.com/quailjs/quail/pull/319)!
 	// `definitionListsAreUsed`, // Would be handy if only we'd support dl/dt/dd.
 	'tableUsesScopeForRow',
 ];
